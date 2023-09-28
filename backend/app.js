eb8194d89d839bb06bb7f8783faa25545165d8db
harshit2467 <92702495+harshit2467@users.noreply.github.com>
2023-09-29 02:20:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,9 +24,9 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/index.html"));
 });
 //middle ware forErrors
 app.use(errorMiddleware);
